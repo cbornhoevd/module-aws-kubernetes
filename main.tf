6987e08f58c66b99edfe09a495dca6db252f5757
bornhoevd <cbornhoevd@gmail.com>
2025-01-10 06:29:49 -0800

    Added variables.tf and updated Jupiter docu accordingly

--- main.tf
+++ main.tf
@@ -29,31 +29,31 @@ POLICY
 # Cluster Policy
 resource "aws_iam_role_policy_attachment" "jupiter-cluster-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role = aws_iam_role.jupiter-cluster.name
+  role       = aws_iam_role.jupiter-cluster.name
 }
 
 # EC2 Security Group to allow networking traffic with EKS cluster
 resource "aws_security_group" "jupiter-cluster" {
-  name = local.cluster_name
+  name        = local.cluster_name
   description = "Cluster communication with worker nodes"
-  vpc_id = var.vpc_id
+  vpc_id      = var.vpc_id
 
   ingress {
     description = "Inbound traffic from within the security group"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    self = true
+    self        = true
   }
 
   egress {
     description = "Allows unrestricted outbound traffic"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    self = true
+    self        = true
   }
 
   tags = {
@@ -63,12 +63,12 @@ resource "aws_security_group" "jupiter-cluster" {
 
 # EKS Cluster Definition
 resource "aws_eks_cluster" "jupiter" {
-  name = local.cluster_name
+  name     = local.cluster_name
   role_arn = aws_iam_role.jupiter-cluster.arn
 
   vpc_config {
     security_group_ids = [aws_security_group.jupiter-cluster.id]
-    subnet_ids = var.cluster_subnet_ids
+    subnet_ids         = var.cluster_subnet_ids
   }
 
   depends_on = [aws_iam_role_policy_attachment.jupiter-cluster-AmazonEKSClusterPolicy]
@@ -97,33 +97,33 @@ POLICY
 # Node Policy
 resource "aws_iam_role_policy_attachment" "jupiter-node-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role = aws_iam_role.jupiter-node.name
+  role       = aws_iam_role.jupiter-node.name
 }
 
 resource "aws_iam_role_policy_attachment" "jupiter-node-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role = aws_iam_role.jupiter-node.name
+  role       = aws_iam_role.jupiter-node.name
 }
 
 resource "aws_iam_role_policy_attachment" "jupiter-node-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role = aws_iam_role.jupiter-node.name
+  role       = aws_iam_role.jupiter-node.name
 }
 
 # Node Group Definition
 resource "aws_eks_node_group" "jupiter-node-group" {
-  cluster_name = aws_eks_cluster.jupiter.name
+  cluster_name    = aws_eks_cluster.jupiter.name
   node_group_name = "jupiter"
-  node_role_arn = aws_iam_role.jupiter-node.arn
-  subnet_ids = var.nodegroup_subnet_ids
+  node_role_arn   = aws_iam_role.jupiter-node.arn
+  subnet_ids      = var.nodegroup_subnet_ids
 
   scaling_config {
     desired_size = var.nodegroup_desired_size
-    max_size = var.nodegroup_max_size
-    min_size = var.nodegroup_min_size
+    max_size     = var.nodegroup_max_size
+    min_size     = var.nodegroup_min_size
   }
 
-  disk_size = var.nodegroup_disk_size
+  disk_size      = var.nodegroup_disk_size
   instance_types = var.nodegroup_instance_types
 
   depends_on = [
@@ -135,7 +135,7 @@ resource "aws_eks_node_group" "jupiter-node-group" {
 
 # Create kubeconfig file based on EKS Cluster Definition
 resource "local_file" "kubeconfig" {
-  content = <<KUBECONFIG
+  content  = <<KUBECONFIG
 apiVersion: v1
 clusters:
 - cluster:
